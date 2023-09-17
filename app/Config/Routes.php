d7dc42fcdbf924b265f53f04ad9c2bdf550259c7
rustom <rustomliquigan23@gmail.com>
2023-09-17 07:32:03 -0700

    view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/arkieno', 'Home::arkieno');
+$routes->get('/product', 'ProductController::arkieno');
+$routes->get('/product/(:any)', 'ProductController::product/1$');
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,52 +6,19 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+
+    }
+    public function arkieno()
+    {
+        return view('products');
+
+    }
     public function index()
     {
-        $main = new ProductModel();
-        $data['data'] = $main->findAll();
-        return view('main', $data);
+
     }
 
-    public function save()
-    {
-        $main = new ProductModel;
-        $data = [
-            'UPC' => $this->request->getVar('UPC'),
-            'ProductName' => $this->request->getVar('ProductName'),
-            'Price' => $this->request->getVar('Price'),
-            'Quantity' => $this->request->getVar('Quantity'),
-            'Create_at' => $this->request->getVar('Create_at'),
-        ];
-        $main = new ProductModel();
-        $main->save($data);
-        return redirect()->to('/');
-    }
-
-    public function delete($id)
-    {
-        $main = new ProductModel;
-        $product = $main->find($id);
-
-        if (!$product){
-            
-            return redirect()->to('/error');
-
-
-        }
-
-        $main->delete($id);
-
-        return redirect()->to('/');
-
-
-        }
-        public function add()
-        {
-            $model=new ProductModel();
-            $data['data']= $model->select('id,ProductName')->findAll();
-            return view('add',$data);
-                
-        }   
-
-    }   
+}   
